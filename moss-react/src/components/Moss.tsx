5850311f929c38615320b59797272c55df19c9d1
Heejae-Kim <heejjeeh@gmail.com>
2021-07-13 00:49:02 +0900

    stack

--- moss-react/src/components/Moss.tsx
+++ moss-react/src/components/Moss.tsx
@@ -1,10 +1,20 @@
 import { socketUri } from "../Socket";
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
+import { Stack } from "../lib/stack";
+
+//create your forceUpdate hook
+function useForceUpdate() {
+  const [value, setValue] = useState(0); // integer state
+  return () => setValue((value) => value + 1); // update the state to force render
+}
 
 export const Moss = () => {
   const [room, setRoom] = useState("1");
   const [socket, setSocket] = useState(io.connect(socketUri));
+  const [stack, setStack] = useState(new Stack<string>());
+  const [msg, setMsg] = useState("");
+  const forceUpdate = useForceUpdate();
 
   useEffect(() => {
     console.log("useEffect");
@@ -17,8 +27,9 @@ export const Moss = () => {
     socket.on(`msgToClient`, receiveMessage);
   }, []);
 
-  const receiveMessage = (message: string) => {
+  const receiveMessage = (message: any) => {
     console.log(message);
+    setMsg(message.msg);
   };
 
   const join = () => {
@@ -27,14 +38,44 @@ export const Moss = () => {
   };
 
   const sendMessage = (e: any) => {
-    const response = socket.emit(`msgToServer`, { room, msg: "test" });
+    const response = socket.emit(`msgToServer`, {
+      room,
+      msg: stack.toString(),
+    });
     console.log(response);
+  };
+
+  const handleDot = (e: any) => {
+    setStack(stack.push("."));
+    forceUpdate();
+  };
+
+  const handleUnderscore = (e: any) => {
+    setStack(stack.push("_"));
+    forceUpdate();
+  };
+
+  const handleClear = () => {
+    setStack(stack.clear());
+    forceUpdate();
   };
 
   return (
     <>
       <p>Moss</p>
-      <button onClick={sendMessage}>tick</button>
+      <p>보낼 메세지 : {stack.toString()}</p>
+      <p>받은 메세지 : {msg}</p>
+      <br />
+      <button onClick={sendMessage}>send</button>
+      <br />
+      <br />
+      <button onClick={handleDot}>.</button>
+      <br />
+      <br />
+      <button onClick={handleUnderscore}>_</button>
+      <br />
+      <br />
+      <button onClick={handleClear}>clear</button>
     </>
   );
 };
---
+++ moss-react/src/lib/stack.ts
@@ -0,0 +1,30 @@
+export class Stack<T> {
+  private _list: T[] = [];
+
+  public push(element: T) {
+    this._list.push(element);
+    return this;
+  }
+  public pop(): T {
+    const length = this.size;
+    if (length === 0) {
+      throw new Error("length is 0");
+    }
+    const head = this._list[length - 1];
+    this._list = this._list.slice(0, length - 1);
+    return head;
+  }
+  public clear() {
+    this._list = [];
+    return this;
+  }
+  public toString(): string {
+    return this._list.join("");
+  }
+  public indexOf(element: T) {
+    return this._list.indexOf(element);
+  }
+  public get size() {
+    return this._list.length;
+  }
+}
